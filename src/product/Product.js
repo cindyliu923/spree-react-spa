5af6fe0eb32287dbd991a1f98ba174d35d7b0f97
Cindy Liu <cindyliu923@gmail.com>
2020-11-02 23:49:12 +0800

    Update ['attributes'] to .attributes

--- src/product/Product.js
+++ src/product/Product.js
@@ -7,7 +7,7 @@ import { addItem } from '../cart/api';
 const Product = ({ product }) => {
   const dispatch = useDispatch();
   const productImages = useSelector(state => state.products.productImages);
-  const image = productImages.find((image) => image['attributes'].viewable_id === Number(product.id))
+  const image = productImages.find((image) => image.attributes.viewable_id === Number(product.id))
   const addItemToCart = async (product) =>  {
     await dispatch(addItem(product));
     const element = document.getElementById(product.relationships.default_variant.data.id);
@@ -18,7 +18,7 @@ const Product = ({ product }) => {
     <div className="col s12 m6 l3">
       <div className="card">
         <div className="card-image">
-          <img src={ image ? HOST + image['attributes'].styles[2].url : defaultImage } alt="product" />
+          <img src={ image ? HOST + image.attributes.styles[2].url : defaultImage } alt="product" />
           <button className="btn-floating btn-large halfway-fab waves-effect waves-light red sidenav-trigger show-on-large" 
             data-target="slide-out"
             onClick={() => addItemToCart(product)}>
@@ -26,9 +26,9 @@ const Product = ({ product }) => {
           </button>
         </div>
         <div className="card-content">
-          <span className="card-title grey-text text-darken-4 truncate">{product['attributes'].name}</span>
-          <div className="card-title card-action">{product['attributes'].display_price}</div>
-          <p className="truncate">{product['attributes'].description}</p>
+          <span className="card-title grey-text text-darken-4 truncate">{product.attributes.name}</span>
+          <div className="card-title card-action">{product.attributes.display_price}</div>
+          <p className="truncate">{product.attributes.description}</p>
         </div>
       </div>
     </div>
